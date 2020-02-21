884cf893bfb923cf73e2d475c15a2f5e5fcb5f92
ddimm <dcd2sg@virginia.edu>
2020-02-20 21:53:27 -0500

    refactor code with better names

--- scripts/send_recv.py
+++ scripts/send_recv.py
@@ -3,66 +3,73 @@ import rospy
 import serial
 import traceback
 from hero_board.msg import MotorVal
-from hero_board.srv import ControlServer, ControlServerResponse, GetState, GetStateResponse
+from hero_board.srv import GetState, GetStateResponse, SetState, SetStateRequest, SetStateResponse
 from utils.protocol import var_len_proto_recv, var_len_proto_send
 
 MOTOR_REC_NAME = "motor_commands"
 MOTOR_COMMAND_PUB = "/motor/output"
 MOTOR_VOLT_NAME = "/motor/current"
 AI_MOTOR_CONTROL = "/motor/cmd_vel"
-manual_sub=None
-auto_sub =None
-current_state = 'manual'
+
+manual_sub = None
+auto_sub = None
+current_state = SetStateRequest.MANUAL
+
 try:
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=None)
 except Exception as e:
     traceback.print_exc()
     exit(-1)
+
+
 def handle_state_request(req):
     global current_state
     return GetStateResponse(current_state)
+
+    
 def process_motor_values(motor_vals):
     '''
     takes in the MotorVal message as a parameter and sends the bytes
     to serial
     '''
     m_val = motor_vals.motorval
-    rospy.loginfo("motor value: %s", m_val)
+    rospy.loginfo("motor value manual: %s", m_val)
     ser.write(var_len_proto_send(m_val))
+
 
 def process_auto_motor_values(motor_vals):
     m_val = motor_vals.motorval
-    rospy.loginfo('motor value: %s', m_val)
+    rospy.loginfo('motor value autonomy: %s', m_val)
     ser.write(var_len_proto_send(m_val))
 
+
 def change_control(req):
-    global auto_sub
-    global manual_sub
-    global current_state
-    if req.changeControl == 'manual':
+    global auto_sub, manual_sub, current_state
+
+    to_change = req.state
+    if to_change == current_state:
+        return SetStateResponse("no change in state")
+
+    if to_change == SetStateRequest.MANUAL:
         print('changing to manual control')
         if auto_sub:
             auto_sub.unregister()
         manual_sub = rospy.Subscriber(MOTOR_COMMAND_PUB, MotorVal, process_motor_values)
-        current_state = 'manual'
-        return ControlServerResponse('changing to manual control')
+        return SetStateResponse('changing to manual control')
 
-    elif req.changeControl == 'automatic':
-        print('changing to automatic')
-        current_state='automatic'
+    elif to_change == SetStateRequest.AUTONOMY:
+        print('changing to autonomy')
         if manual_sub:
             manual_sub.unregister()
         
         auto_sub = rospy.Subscriber(AI_MOTOR_CONTROL, MotorVal, process_auto_motor_values)
-        return ControlServerResponse('changing to automatic control')
+        return SetStateResponse('changing to autonomy control')
 
-
+    current_state = to_change
 
 def control_server():
-    serv = rospy.Service("change_control", ControlServer, change_control)
-    print("server started")
-    state_serv = rospy.Service('current_state', GetState, handle_state_request)
-    print('started state service')
+    serv = rospy.Service("/set_state", SetState, change_control)
+    state_serv = rospy.Service("/get_state", GetState, handle_state_request)
 
 if __name__ == "__main__":
     try:
@@ -72,9 +79,9 @@ if __name__ == "__main__":
         '''
         control_server()
         rospy.init_node(MOTOR_REC_NAME, anonymous=True)
-        manual_sub =rospy.Subscriber(MOTOR_COMMAND_PUB, MotorVal, process_motor_values)
+        manual_sub = rospy.Subscriber(MOTOR_COMMAND_PUB, MotorVal, process_motor_values)
         
-        print("starting publisher")
+        rospy.loginfo("starting publisher")
         # no need to used a thread here. As per testing, main thread works fine
         # ros publisher queue can be used to limit the number of messages
         pub = rospy.Publisher(MOTOR_VOLT_NAME, MotorVal, queue_size=5)
